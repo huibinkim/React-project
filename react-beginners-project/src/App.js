36236e35aefae8eeb377205f343e6f8628282958
huibinkim <94955669+huibinkim@users.noreply.github.com>
2023-04-05 13:40:26 +0900

    useEffect

--- react-beginners-project/src/App.js
+++ react-beginners-project/src/App.js
@@ -1,9 +1,22 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  // console.log("render");
+  //내가 state를 변경할 때 모든 코드들은 항상 다시 실행된다. 콘솔에 렌더가 여러번 찍히는것처럶.
+  //useEffect : 두개의 argument 를 가지는 function이다. 첫번째 argument는 우리가 딱 한번만 실행하고픈 코드
+  console.log("i run all");
+  // const iRunOnce = () => {
+  //   console.log("one");
+  // };
+  useEffect(() => console.log("call the API"), []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome!!</h1>
+      <h1 className={styles.title}>{counter}!!</h1>
+      <button onClick={onClick}>click me</button>
       <Button text={"btn"} />
     </div>
   );
--- react-beginners-project/src/index.js
+++ react-beginners-project/src/index.js
@@ -3,9 +3,5 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 // 여기서 app.js를 import시켜 html에 보여준다.
