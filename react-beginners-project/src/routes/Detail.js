6f1903b9fc6e1a5db92954d9d036c72c5e9d0c15
huibinkim <94955669+huibinkim@users.noreply.github.com>
2023-04-11 16:32:01 +0900

    console에 있는 detail 내용 화면으로 뿌리고, home 버튼도 만듬.

--- react-beginners-project/src/routes/Detail.js
+++ react-beginners-project/src/routes/Detail.js
@@ -1,18 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [details, setDetail] = useState([]);
   // console.log(id);
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setDetail(json.data.movie);
+    setLoading(false);
   };
   useEffect(() => {
     getMovie();
-  });
-  return <h1>Detail</h1>;
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>로딩중</h1>
+      ) : (
+        <div>
+          <button>
+            <Link to={`/`}>Go home</Link>
+          </button>
+          <h1>{details.title}</h1>
+          <img src={details.background_image_original} />
+          <hr></hr>
+          <h2>
+            {details.genres} ({details.year}): {details.rating}
+          </h2>
+          <p>{details.description_full}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 export default Detail;
 //useParam 은 리액트 라우터에서 제공하는 함수.
