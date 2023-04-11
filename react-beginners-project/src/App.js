1bb02d218bdb9140a6597464abfbe212204a71a9
huibinkim <94955669+huibinkim@users.noreply.github.com>
2023-04-11 15:34:30 +0900

    app.js에서 하던 것들을 component화 시켜 옮겨주고 url이 바뀌면 보여줄 것들을 적어놓음.

--- react-beginners-project/src/App.js
+++ react-beginners-project/src/App.js
@@ -1,4 +1,22 @@
-function App() {
-  return null;
-}
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+//Route는 기본 url 뒤 /url Url을 의미한다.
+
+const App = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/movie",
+    element: <Detail />,
+  },
+  {
+    path: "/hello",
+    element: <h1>hi</h1>,
+  },
+]);
+
 export default App;
